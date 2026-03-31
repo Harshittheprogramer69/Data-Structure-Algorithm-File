c469a3c9fc365e7ccf749713adc1a02666a89e2f
Harshit_khanna <117803004+Harshittheprogramer69@users.noreply.github.com>
2026-03-31 13:53:59 +0530

    DSA FILE COMPLETED

--- Codes/37.TraversalTree.c
+++ Codes/37.TraversalTree.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 struct node {
     int data;
     struct node *left;
     struct node *right;
 };
-
 struct node* createNode(int value) {
     struct node* newNode;
     newNode = (struct node*)malloc(sizeof(struct node));
-
     newNode->data = value;
     newNode->left = NULL;
     newNode->right = NULL;
-
     return newNode;
 }
-
 void INORDER(struct node* root) {
     if(root != NULL) {
         INORDER(root->left);
@@ -25,7 +20,6 @@ void INORDER(struct node* root) {
         INORDER(root->right);
     }
 }
-
 void PREORDER(struct node* root) {
     if(root != NULL) {
         printf("%d ", root->data);
@@ -33,7 +27,6 @@ void PREORDER(struct node* root) {
         PREORDER(root->right);
     }
 }
-
 void POSTORDER(struct node* root) {
     if(root != NULL) {
         POSTORDER(root->left);
@@ -41,25 +34,18 @@ void POSTORDER(struct node* root) {
         printf("%d ", root->data);
     }
 }
-
 int main() {
-
     struct node* root;
-
     root = createNode(1);
     root->left = createNode(2);
     root->right = createNode(3);
     root->left->left = createNode(4);
     root->left->right = createNode(5);
-
     printf("Inorder Traversal:\n");
     INORDER(root);
-
     printf("\n\nPreorder Traversal:\n");
     PREORDER(root);
-
     printf("\n\nPostorder Traversal:\n");
     POSTORDER(root);
-
     return 0;
 }
